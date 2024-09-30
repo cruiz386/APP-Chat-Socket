f561a95a464a1ca71b9820f499c210fecb3065a6
cruiz386 <cruiz@teco.com.ar>
2024-09-30 12:30:41 -0300

    vistas

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,5 +1,11 @@
 import { Router } from 'express';
+import viewRouter from './views/index.views.js'
+import apiRouter from './api/index.api.js'
+
 
 const router = Router();
+
+router.use("/", viewRouter)
+router.use ("/api", apiRouter)
 
 export default router
